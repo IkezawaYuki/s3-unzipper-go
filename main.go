74e6833b600ccc285ee7e200783be28a3f107a8f
yikezawa <y.ikezawa93@gmail.com>
2020-02-09 15:59:41 +0900

    mod

---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+const (
+	tempArtifactPath = "/tmp/artifact/"
+	tempZipPath      = tempArtifactPath + "zipped/"
+	tempUnzipPath    = tempArtifactPath + "unzipped/"
+	tempZip          = "temp.zip"
+	dirPerm          = 0777
+	region           = endpoints.ApNortheast1RegionID
+)
+
+var (
+	now              string
+	zipContentPath   string
+	unzipContentPath string
+	deskBucket       string
+)
+
+func init() {
+	deskBucket = os.Getenv("UNZIP_ARTIFACT_BUCKET")
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, s3event events.S3Event) error {
+	if lc, ok := lambdacontext.FromContext(ctx); ok {
+		log.Printf("AwsRequestID: %v", lc.AwsRequestID)
+	}
+
+	bucket := s3event.Records[0].S3.Bucket.Name
+	key := s3event.Records[0].S3.Object.Key
+
+	log.Printf("Bucket: %v, Key: %v", bucket, key)
+
+	if err := prepareDirectory(); err != nil {
+		log.Fatal(err)
+	}
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(region),
+	}))
+
+	downloader := s3.NewDownloader(sess, bucket, key, zipContentPath+tempZip)
+
+	return nil
+}
+
+func prepareDirectory() error {
+	now := strconv.Itoa(int(time.Now().UnixNano()))
+	zipContentPath = tempZipPath + now + "/"
+	unzipContentPath = tempUnzipPath + now + "/"
+	if _, err := os.Stat(tempArtifactPath); err == nil {
+		if err := os.RemoveAll(tempArtifactPath); err != nil {
+			return err
+		}
+	}
+
+	if err := os.MkdirAll(zipContentPath, dirPerm); err != nil {
+		return err
+	}
+
+	if err := os.MkdirAll(unzipContentPath, dirPerm); err != nil {
+		return err
+	}
+
+	return nil
+}
