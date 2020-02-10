9e6569ea278a332abbd5cb6a6603eb2db31e54cd
yikezawa <y.ikezawa93@gmail.com>
2020-02-10 19:31:04 +0900

    mod

--- s3/uploader.go
+++ s3/uploader.go
@@ -1,9 +1,13 @@
 package s3
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"golang.org/x/sync/errgroup"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 type Uploader struct {
@@ -23,6 +27,25 @@ func NewUploader(s *session.Session, src string, dest string) *Uploader {
 func (u Uploader) Upload() error {
 	eg := errgroup.Group{}
 
+	err := filepath.Walk(u.src, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+
+		eg.Go(func() error {
+			file, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			key := strings.Replace(file.Name(), u.src, "", 1)
+		})
+	})
 	// todo
 
 	return nil
